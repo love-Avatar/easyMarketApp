9b753d3140cde608e22d754f0493caca7643d9b1
dreamSeekerYu <3024637102@qq.com>
2019-09-23 20:24:42 +0800

    登陆完成

--- src/store/login.ts
+++ src/store/login.ts
@@ -1,37 +1,26 @@
+import { login } from '@/service'
 
-import request from '@/utils/request';
 export default {
   namespaced: true,
 
   state: {
-    routeUrl: 1234567
+    loginMSG: null
   },
   getters: {
-    routeUrl(state: any) {
-      // return state.routeUrl || storage.get('productList__routeUrl', null)
-    }
+
   },
   mutations: {
-    routeUrl(state: any, url: any) {
-      state.routeUrl = url
-      // storage.set("productList__routeUrl", url)
-    }
+    // login(state: any, loginMSG: any) {
+    //   console.log(JSON.stringify(state))
+    //   state.loginMSG = loginMSG
+    //   console.log(JSON.stringify(state))
+    // }
   },
   actions: {
-    async init({ commit, state }: { commit: any, state: any }, pid: any) {
-      // await request.get(url + '', params: { pid }).then((res) => {
-      //   state.all = res.all
-      //   commit('resolve', res.data)
-      // })
-      await request.get('url').then((res: any) => {
-        state.all = res.all
-        commit('resolve', res.data)
-      })
+    async login({ commit, state }: { commit: any, state: any }, opt: any) {
+      const result = await login(opt)
+      // commit('login', result)
+      return result
     }
   }
 }
-
-
-// 页面调用   this.$store.dispatch('xxxx/fetchList');
-
-// 改造命名空间的写法  参考  https://www.jianshu.com/p/80203ed29549
